e8fa9f9be7050e878ec3d3b36661aadc9f31be5a
silokilo966 <silokilo966@gmail.com>
2021-08-03 03:45:54 -0700

    Completed

--- lib/theme_settings.dart
+++ lib/theme_settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 ThemeData lightTheme = ThemeData.light();
 ThemeData darkTheme = ThemeData.dark();
@@ -6,9 +7,71 @@ ThemeData darkTheme = ThemeData.dark();
 class ThemeSettings with ChangeNotifier {
   bool _darkMode = false;
   bool get darkMode => _darkMode;
+  SharedPreferences? _preferences;
+
+  ThemeSettings() {
+    _loadPrefs();
+  }
+
+  _initializePrefs() async {
+    if (_preferences == null) {
+      _preferences = await SharedPreferences.getInstance();
+    }
+  }
+
+  _savePrefs() async {
+    await _initializePrefs();
+    _preferences?.setBool('ThemeMode', _darkMode);
+  }
+
+  _loadPrefs() async {
+    await _initializePrefs();
+    _darkMode = _preferences?.getBool('ThemeMode') ?? false;
+    notifyListeners();
+  }
 
   toggleDarkMode() {
     _darkMode = !_darkMode;
+    _savePrefs();
     notifyListeners();
   }
 }
+
+//todo User Shared preferences to store the ThemMode data!
+
+
+
+
+
+
+
+
+
+//SharedPreferences? _preferences;
+
+  // ThemeSettings() {
+  //   _loadThemePrefs();
+  // }
+
+  // _initializePrefs() async {
+  //   if (_preferences == null) {
+  //     _preferences = await SharedPreferences.getInstance();
+  //   }
+  // }
+
+  // _saveThemePrefs() async {
+  //   await _initializePrefs();
+  //   _preferences?.setBool('ThemeKey', _darkMode);
+  // }
+
+  // _loadThemePrefs() async {
+  //   await _initializePrefs();
+  //   _darkMode = _preferences?.getBool('ThemeKey') ?? false;
+  // }
+
+//   toggleDarkMode() {
+//     _darkMode = !_darkMode;
+//     _saveThemePrefs();
+//     notifyListeners();
+//   }
+// }
